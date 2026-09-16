4001aa1dc3fdd1c5141ce33a38d1aaf6df8f6c35
Hadrien Hamel <hadrien.hamel@gmail.com>
2026-09-16 16:47:39 +0200

    Fix after updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,27 @@
 import datetime
 
 from flask import Flask
-from flask.json import JSONEncoder
+from flask.json.provider import DefaultJSONProvider
 from flask_bootstrap import Bootstrap4
-from flask_fontawesome import FontAwesome
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from markupsafe import Markup
 
 from config import config
 
 db = SQLAlchemy()
 bootstrap = Bootstrap4()
-fa = FontAwesome()
 migrate = Migrate()
 
-class CustomJSONEncoder(JSONEncoder):
+
+def fontawesome_html():
+    return Markup(
+        '<link rel="stylesheet" '
+        'href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" '
+        'crossorigin="anonymous">'
+    )
+
+class CustomJSONProvider(DefaultJSONProvider):
     def default(self, obj):
         if isinstance(obj, datetime.date):
             return obj.strftime('%Y-%m-%d')
@@ -23,12 +30,13 @@ class CustomJSONEncoder(JSONEncoder):
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.json_encoder = CustomJSONEncoder
+    app.json_provider_class = CustomJSONProvider
+    app.json = app.json_provider_class(app)
 
     app.config.from_object(config[config_name])
 
     bootstrap.init_app(app)
-    fa.init_app(app)
+    app.jinja_env.globals["fontawesome_html"] = fontawesome_html
     db.init_app(app)
     migrate.init_app(app, db, render_as_batch=True)
 
@@ -42,4 +50,3 @@ def create_app(config_name):
     app.register_blueprint(api_bp)
 
     return app
-
